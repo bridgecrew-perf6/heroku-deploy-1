11925cf7dc6aedeb25742aa6ac564041debead16
jayasai183 <jayasaivardhan.jv@gmail.com>
2022-06-25 13:19:15 +0530

    added path module

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const path=require('path');
 const api=require("./server/api");
 
 app.use('/api',api);
